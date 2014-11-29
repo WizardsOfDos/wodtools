be3ecc1b9000ca1691ef4216da18a0ea478f8cb8
Felix Rohrbach <fxrh@gmx.de>
2014-11-29 03:14:08 +0100

    database: function to save events

--- ctf-submitter/database.py
+++ ctf-submitter/database.py
@@ -1,5 +1,24 @@
-def saveEvent( event, flag, data ):
-    pass
+from elasticsearch import Elasticsearch
+
+ES_INDEX="ctf-submitter"
+ES_TYPE_EVENT="event"
+ES_HOST="localhost"
+ES_PORT=9200
+
+def createConnection():
+    return ElasticSearch([{"host": ES_HOST, "port": ES_PORT}])
+
+
+def addEventEntry(connection, event, flag, additionalData={}):
+    additionalData["event"] = event
+    additionalData["flag"] = flag
+    return _createEvent(connection, additionalData)
+
 
 def getEvents( event, flag ):
     pass
+
+
+def _createEvent(es, data):
+    res = es.index(index=ES_INDEX, doc_type=ES_TYPE_EVENT, body=data)
+    return res['_id']
