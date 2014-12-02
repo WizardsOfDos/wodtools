753e13af1fcdc25f62b1283ce4aa9bc3a449909e
Patrick Welzel <patrick@kcirtap.de>
2014-12-02 13:32:41 +0100

    added CELERY_IMPORT to make all the modules available

--- flgproc/celeryconfig.py
+++ flgproc/celeryconfig.py
@@ -3,8 +3,14 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_ACCEPT_CONTENT=['json']
+CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TIMEZONE = 'Europe/Oslo'
 CELERY_ENABLE_UTC = True
 
+CELERY_IMPORTS = ['flgproc.%s' % x for x in [
+    'filter.duplicate',
+    'filter.matching',
 
+    'collector.demo',
+    'submitter.demo',
+]]
