04f6cc3fa488a805b6134b787020500c91b77408
Patrick Welzel <patrick@kcirtap.de>
2014-12-04 18:31:50 +0100

    Renamed IllegalFlagException to MalformedFlagException

--- flgproc/exceptions.py
+++ flgproc/exceptions.py
@@ -5,11 +5,13 @@ class FlagException(Exception):
         self.reason = 'Unknown'
 
 
-class IllegalFlagException(FlagException):
-    def __init__(self, flag, pattern):
-        super(IllegalFlagException, self).__init__(flag,
-            "Flag '{flag}' does not match the tournaments flag pattern '{pattern}'".format(flag=flag, pattern=pattern))
-        self.reason = 'illegalFlag'
+class MalformedFlagException(FlagException):
+    def __init__(self, flag, constraint_notes=None):
+        text = "Flag '{flag}' does not match the constraints on flag format".format(flag=flag)
+        if constraint_notes is not None:
+            text = "{text} ({constraint_notes})".format(text=text, constraint_notes=constraint_notes)
+        super(MalformedFlagException, self).__init__(flag, text)
+        self.reason = 'malformedFlag'
 
 
 class DuplicateFlagException(FlagException):
--- flgproc/filter/matching.py
+++ flgproc/filter/matching.py
@@ -4,14 +4,15 @@ from celery.utils.log import get_task_logger
 
 from flgproc import conf
 from flgproc.tasks import app
-from flgproc.exceptions import IllegalFlagException
+from flgproc.exceptions import MalformedFlagException
 
 
 logger = get_task_logger(__name__)
+
 
 @app.task()
 def regex(flag, pattern=conf.FLAG_PATTERN):
     logger.debug("checking flag '{0}' for matching regex pattern".format(flag))
     if re.fullmatch(pattern, flag) is None:
-        raise IllegalFlagException(flag, pattern)
+        raise MalformedFlagException(flag, "pattern: '%s'" % pattern)
     return flag
