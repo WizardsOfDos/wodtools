a4b6bf08f63157692601f27e64f18ac4d4b66168
Patrick Welzel <patrick@kcirtap.de>
2014-12-04 10:49:08 +0100

    added base class for flgproc exceptions

--- flgproc/exceptions.py
+++ flgproc/exceptions.py
@@ -1,9 +1,18 @@
-class IllegalFlagException(Exception):
+class FlagException(Exception):
+    def __init__(self, flag, text):
+        super(FlagException, self).__init__(text)
+        self.flag = flag
+        self.reason = 'Unknown'
+
+
+class IllegalFlagException(FlagException):
     def __init__(self, flag, pattern):
-        super(IllegalFlagException, self).__init__(
+        super(IllegalFlagException, self).__init__(flag,
             "Flag '{flag}' does not match the tournaments flag pattern '{pattern}'".format(flag=flag, pattern=pattern))
+        self.reason = 'illegalFlag'
 
 
-class DuplicateFlagException(Exception):
+class DuplicateFlagException(FlagException):
     def __init__(self, flag):
-        super(DuplicateFlagException, self).__init__("Duplicate flag: '{flag}'".format(flag=flag))
+        super(DuplicateFlagException, self).__init__(flag, "Duplicate flag: '{flag}'".format(flag=flag))
+        self.reason = 'duplicateFlag'
