512f95f15234b8417d2070395426537784408d6d
Patrick Welzel <patrick@kcirtap.de>
2014-12-05 22:32:01 +0100

    add basic submitter via plain UDP packets

--- flgproc/submitter/demo.py
+++ flgproc/submitter/demo.py
@@ -1,11 +1,22 @@
+import socket
+
 from celery.utils.log import get_task_logger
 
 from flgproc.tasks import app
 
 logger = get_task_logger(__name__)
-
+_UDPSOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 @app.task(ignore_result=True)
 def printer(flag, *args, **kwargs):
     logger.info("Flag {flag} was submitted with the following args({args}) and kwargs({kwargs})".format(
         flag=flag, args=args, kwargs=kwargs))
+
+
+@app.task(ignore_result=True)
+def send_udp(flag, host='127.0.0.1', port=4223, msg=None):
+    if msg is None:
+        msg = "{flag}\n"
+    DATA=msg.format(flag=flag)
+    logger.debug("sending flag '{data}' to ({host}, {port})".format(data=DATA, host=host, port=port))
+    _UDPSOCK.sendto(bytes(DATA, 'utf-8'), (host, port))
